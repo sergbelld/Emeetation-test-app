efa2a1bcd4f3a6bf6cfe206c96558c6886250e4e
Sergey Belikov <66332298+sergbelld@users.noreply.github.com>
2020-11-16 07:50:01 +0300

    Update interests_screen.dart

--- lib/pages/interests_screen.dart
+++ lib/pages/interests_screen.dart
@@ -57,7 +57,7 @@ class _InterestsScreenState extends State<InterestsScreen> {
                   child: Padding(
                     padding: Platform.isIOS
                         ? const EdgeInsets.only(right: 14.0)
-                        : const EdgeInsets.only(right: 0.0),
+                        : const EdgeInsets.only(right: 22.0),
                     child: FloatingActionButton(
                       backgroundColor: Theme.of(context).primaryColor,
                       elevation: 0.0,
